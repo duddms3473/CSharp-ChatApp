f485b9385a0461ad9ca91afe6313ebf96c504e34
Lee Young Eun <77182479+duddms3473@users.noreply.github.com>
2023-10-20 01:57:34 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace tongsingo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(534, 764);
+            this.splitContainer1.SplitterDistance = 545;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("나눔스퀘어OTF", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(534, 545);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(534, 214);
+            this.splitContainer2.SplitterDistance = 160;
+            this.splitContainer2.SplitterWidth = 5;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(534, 160);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.splitContainer3.Panel1.Controls.Add(this.button2);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.button1);
+            this.splitContainer3.Size = new System.Drawing.Size(534, 49);
+            this.splitContainer3.SplitterDistance = 458;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("나눔스퀘어OTF", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "전송";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(417, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 49);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "연결";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(534, 764);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("나눔스퀘어OTF", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "채팅 앱";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace tongsingo
+{
+    public partial class Form1 : Form
+    {
+        private string serverIP = "127.0.0.1"; // 서버의 IP 주소
+        private string clientIP = "127.0.0.1";
+        private int serverPort = 12345; // 서버의 포트 번호
+        private int clientPort = 12346; // 클라이언트의 포트 번호
+
+        private Thread listenThread;
+        private Socket clientSocket;
+        private Socket serverSocket;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Log(string msg)
+        {
+            string timeString = DateTime.Now.ToString("HH:mm");
+            if (richTextBox1.InvokeRequired)
+            { 
+                richTextBox1.Invoke(new Action( () => richTextBox1.SelectionAlignment = HorizontalAlignment.Left));
+                richTextBox1.Invoke(new Action(() => richTextBox1.AppendText(string.Format("{0}  [{1}]", msg, timeString) + "\r\n")));
+            }
+            else
+            {
+                richTextBox1.SelectionAlignment = HorizontalAlignment.Left;
+                richTextBox1.AppendText(string.Format("{0}  [{1}]", msg, timeString) + "\r\n");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listenThread = new Thread(new ThreadStart(Listen));
+            listenThread.IsBackground = true;
+            listenThread.Start();
+        }
+
+        private void ConnectToServer()
+        {
+            try
+            {
+                IPAddress serverIPAddress = IPAddress.Parse(clientIP);
+                IPEndPoint serverEndPoint = new IPEndPoint(serverIPAddress, clientPort);
+
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                clientSocket.Connect(serverEndPoint);
+                Log("서버에 연결되었습니다.");
+            }
+            catch (Exception ex)
+            {
+                Log("서버 연결 중 오류 발생: " + ex.Message);
+            }
+        }
+
+        private void Listen()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, serverPort);
+
+            serverSocket.Bind(localEndPoint);
+            serverSocket.Listen(10);
+
+            Log("클라이언트 요청 대기중...");
+
+            while (true)
+            {
+                Socket connectedClientSocket = serverSocket.Accept();
+                Log("클라이언트 접속됨");
+
+                Thread receiveThread = new Thread(new ParameterizedThreadStart(Receive));
+                receiveThread.IsBackground = true;
+                receiveThread.Start(connectedClientSocket);
+            }
+        }
+
+
+        private void Receive(object clientSocket)
+        {
+            Socket connectedClientSocket = (Socket)clientSocket;
+
+            while (true)
+            {
+                byte[] receiveBuffer = new byte[512];
+                int length = connectedClientSocket.Receive(receiveBuffer, receiveBuffer.Length, SocketFlags.None);
+
+                string msg = Encoding.UTF8.GetString(receiveBuffer, 0, length);
+
+                Log("상대: " + msg);
+            }
+        }
+
+        private void SendToClient(string message)
+        {
+            try
+            {
+                byte[] sendBuffer = Encoding.UTF8.GetBytes(message);
+                clientSocket.Send(sendBuffer);
+                mesend("나: " + message);
+            }
+            catch (Exception ex)
+            {
+                Log("메시지 전송 중 오류 발생: " + ex.Message);
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SendToClient(textBox1.Text.Trim());
+                textBox1.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text.Trim()))
+            {
+                SendToClient(textBox1.Text.Trim());
+                textBox1.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ConnectToServer();
+        }
+
+        private void mesend(string msg)
+        {
+            string timeString = DateTime.Now.ToString("HH:mm");
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Invoke(new Action(() => richTextBox1.SelectionAlignment = HorizontalAlignment.Right));
+                richTextBox1.Invoke(new Action(() => richTextBox1.AppendText(string.Format("[{0}]  {1}", timeString, msg) + "\r\n")));
+            }
+            else
+            {
+                richTextBox1.SelectionAlignment = HorizontalAlignment.Right;
+                richTextBox1.AppendText(string.Format("[{0}]  {1}", timeString, msg) + "\r\n");
+            }
+        }
+    }
+}
